5352cc12880fcded66882fbfae5e3f64e26a0bfe
Nav Marwaha <nma@sitecore.net>
2014-09-23 15:36:17 +0100

    Updated FakeSitecore and nuget packages

--- BasicSitecoreSolution.UnitTests/TestHelper/FakeSitecore.cs
+++ BasicSitecoreSolution.UnitTests/TestHelper/FakeSitecore.cs
@@ -3,6 +3,8 @@ using System.Linq;
 
 namespace BasicSitecoreSolution.UnitTests.TestHelper
 {
+    using System;
+
     using BasicSitecoreSolution.BusinessLogic.Wrappers;
 
     using Microsoft.QualityTools.Testing.Fakes;
@@ -10,6 +12,7 @@ namespace BasicSitecoreSolution.UnitTests.TestHelper
     using Sitecore.Collections;
     using Sitecore.Collections.Fakes;
     using Sitecore.Data;
+    using Sitecore.Data.Fakes;
     using Sitecore.Data.Fields;
     using Sitecore.Data.Fields.Fakes;
     using Sitecore.Data.Items;
@@ -19,10 +22,22 @@ namespace BasicSitecoreSolution.UnitTests.TestHelper
     {
         public Item[] GetPropertyItems()
         {
+
+
+            ItemPath paths1 = new ShimItemPath
+            {
+                PathGet = () => "ItemPath1"
+            };
+
+            ItemPath paths2 = new ShimItemPath
+            {
+                PathGet = () => "ItemPath2"
+            };
+
             return new Item[]
                        {
-                           new ShimItem { NameGet = () => "TestItem1", TemplateNameGet = () => "Semi-Detached House", FieldsGet = () => this.GetTestFieldCollection(1) },
-                           new ShimItem { NameGet = () => "TestItem2", TemplateNameGet = () => "Terraced House", FieldsGet = () => this.GetTestFieldCollection(2), }
+                           new ShimItem {IDGet = () => new ID(Guid.NewGuid()), NameGet = () => "TestItem1", TemplateNameGet = () => "Semi-Detached House", FieldsGet = () => this.GetTestFieldCollection(1), PathsGet = () => paths1},
+                           new ShimItem {IDGet = () => new ID(Guid.NewGuid()), NameGet = () => "TestItem2", TemplateNameGet = () => "Terraced House", FieldsGet = () => this.GetTestFieldCollection(2), PathsGet = () => paths2}
                        };
         }
 
@@ -32,17 +47,25 @@ namespace BasicSitecoreSolution.UnitTests.TestHelper
 
             FakesDelegates.Func<string, Field> names = name => fields.FirstOrDefault(x => x.Name.Equals(name));
 
-            return new ShimFieldCollection { ItemGetString = names };
+            FakesDelegates.Func<Sitecore.Data.Fields.Field[]> fieldcollection = fields.ToArray;
+
+            FakesDelegates.Func<IEnumerator<Field>> fieldEnnumerator = () => fields.GetEnumerator();
+
+            FakesDelegates.Func<int> fieldcount = () => fields.Count;
+
+            return new ShimFieldCollection { ItemGetString = names, CountGet = fieldcount, GetFields = fieldcollection, SystemCollectionsGenericIEnumerableSitecoreDataFieldsFieldGetEnumerator = fieldEnnumerator };
         }
 
         private IEnumerable<Field> Getfields(int i)
         {
             var fields = new List<Field>
                              {
-                                 new ShimField { IDGet = () => new ID(), NameGet = () => "Cost", ValueGet = () => "200000" },
-                                 new ShimField { IDGet = () => new ID(), NameGet = () => "Area", ValueGet = () => "London" },
-                                 new ShimField { IDGet = () => new ID(), NameGet = () => "Number Of Bedrooms", ValueGet = () => i.ToString() },
-                                 new ShimField { IDGet = () => new ID(), NameGet = () => "Post code", ValueGet = () => "Ec1 1ah" }
+                                 new ShimField { IDGet = () => new ID(), NameGet = () => "Cost",               DisplayNameGet = () => "Cost", ValueGet = () => "200000" },
+                                 new ShimField { IDGet = () => new ID(), NameGet = () => "Area",                DisplayNameGet = () => "Area", ValueGet = () => "London" },
+                                 new ShimField { IDGet = () => new ID(), NameGet = () => "Number Of Bedrooms",  DisplayNameGet = () => "Number Of Bedrooms", ValueGet = () => i.ToString() },
+                                 new ShimField { IDGet = () => new ID(), NameGet = () => "Post code",           DisplayNameGet = () => "Post code", ValueGet = () => "Ec1 1ah" },
+                                 new ShimField { IDGet = () => new ID(), NameGet = () => "__Updated By",        DisplayNameGet = () => "__Updated By", ValueGet = () => "Someone" },
+                                 new ShimField { IDGet = () => new ID(), NameGet = () => "Empty Field",         DisplayNameGet = () => "Empty Field", ValueGet = () => null },
                              };
 
             return fields;
